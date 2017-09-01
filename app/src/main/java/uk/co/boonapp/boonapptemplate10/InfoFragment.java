b28b403367337eb8511dd36b844261ed9c7260d1
Dylan Lawless (CA Account) <ca_K1517669@kclad.ds.kcl.ac.uk>
2017-09-01 12:44:34 +0100

    Initial commit

---
+++ app/src/main/java/uk/co/boonapp/boonapptemplate10/InfoFragment.java
@@ -0,0 +1,26 @@
+package uk.co.boonapp.boonapptemplate10;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ca_K1517669 on 01/09/2017.
+ */
+
+public class InfoFragment extends Fragment {
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.info_layout, container, false);
+        return myView;
+    }
+
+}
+
